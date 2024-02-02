b71079613cce1787afa2542c7f235e27fdf22142
agabil4c <agabil4c@gmail.com>
2024-02-02 13:29:23 +0300

    pushing master

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 const Navbar = () => {
   const { user } = useContext(AuthContext);
-
+  console.log(user);
   return (
     <div className="navbar">
       <div className="navContainer">
